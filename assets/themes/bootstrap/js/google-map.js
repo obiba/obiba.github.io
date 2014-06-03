b3bab5530c77cb9074199e1004c033afde7385fc
Ramin Haeri Azad <rhaeri@p3g.org>
2014-06-03 18:45:54 -0400

    Editted About, added temporary content for the product pages

--- assets/themes/bootstrap/js/google-map.js
+++ assets/themes/bootstrap/js/google-map.js
@@ -1,25 +1,21 @@
 function init_map() {
-  var var_location = new google.maps.LatLng(45.496580, -73.580104 );
+  var var_location = new google.maps.LatLng(45.496580, -73.580104);
 
   var var_mapoptions = {
-  center: var_location,
-  zoom: 14
+    center: var_location,
+    zoom: 14
   };
 
-var var_marker = new google.maps.Marker({
-  position: var_location,
-  map: var_map,
-  title:"Maelstrom Research"});
+  var var_marker = new google.maps.Marker({
+    position: var_location,
+    map: var_map,
+    title: "Maelstrom Research"});
 
-var var_map = new google.maps.Map(document.getElementById("map-container"),
-var_mapoptions);
+  var var_map = new google.maps.Map(document.getElementById("map-container"),
+    var_mapoptions);
 
-var_marker.setMap(var_map);
+  var_marker.setMap(var_map);
 
 }
 
 google.maps.event.addDomListener(window, 'load', init_map);
-
-
-var content = getMap({center: 'Fritz-Walter Stadion, Kaiserslautern'});
-$('#example').popover({ content: content })
